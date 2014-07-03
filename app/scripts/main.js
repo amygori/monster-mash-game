3c6471525c0a9c03fa424387da09bde3ac9a6c88
amygori <amydgori@gmail.com>
2014-07-02 21:07:28 -0400

    established constructors, prototypes; began scripting actions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,38 @@
 // CONSTRUCTORS
 // Here is where you'll create your constructors for you player and monster.
-// e.g. function Monster(){};
-// e.g. function Player(){};
+
+// Monster constructor
+function Monster(options) {
+	options:options || {};
+	this.health =  0;
+	this.name = options.name;
+}
+
+// Player constructor
+function Player (options) {
+	options:options || {};
+	this.name = options.name;
+}
 
 
 // GLOBAL VARIABLES
 // You'll also want to create variables for the specific players and monsters.
+var nun = new Player({
+
+	});
+
+var priest = new Player({
+
+});
+
+var Godzilla = new Monster({
+
+});
+
+var Mothra = new Monster({
+
+});
+
 // e.g. var purplePeopleEater = new Monster();
 // e.g. var tinaFey = new Player();
 // e.g. var attackMode = function(target){ Some code that produces an attack - pew, pew! };
@@ -16,3 +43,18 @@
 // This is where jQuery will come into play and where you'll set a lot of your
 // interactions for players and monsters. (e.g. player selection, attack interactions)
 // e.g. $('.purple-people-eater').click(function () { Some code that attacks the monster! };
+var attackMode = function(Monster){ 
+	$('.monster a').click(function (e){
+		e.preventDefault();
+		console.log('WHACK!');
+		if Monster.health<===20 {
+			return Monster.health+5;
+		}
+		else {
+			alert ("You won!");
+			window.prompt ("Play again?");
+		}
+	});
+};
+
+
