18a7146cdd0eacedfcb3341bac9136a9930e97dd
amygori <amydgori@gmail.com>
2014-07-06 17:29:46 -0400

    building templates and trying to get images to show up in html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,16 @@
 //Hide the divs as needed
-$("fight1").hide();
-$("fight2").hide();
-$( "#start" ).click(function( event ) {
-  $( this ).hide();
-  $("#fight1").show();
+$("#fight").hide();
+$( "#start" ).click(function(event) {
+  $("#start").hide();
+  
 });
 
 // CONSTRUCTORS
-// Here is where you'll create your constructors for you player and monster.
+
 
 // Monster constructor
-function Monster(health, name) {
-	this.life = 20;
+function Monster(life, name) {
+	this.life = life;
 	this.name = name;
 	this.whack = $('.monster').click(function (whack){
 		console.log('WHACK!');
@@ -20,55 +19,42 @@ function Monster(health, name) {
 			alert ('Success! Victory is yours!');
 			$("#start").show();
 		};//some code here that will alert win and return to beginning
-}
+})};
 
 // Player constructor
 function Player(name) {
 	this.name = name;
-}
+};
 
 
 // GLOBAL VARIABLES
-// You'll also want to create variables for the specific players and monsters.
-var blackKitteh = new Player({
-	name: 'Black Kitteh';
-	var img = new Image(); 
-	img.src = 'blackKitteh.png';
-});
 
-var gatlingKitteh = new Player({
-	name: 'Gatling Kitteh';
-	var img = new Image(); 
-	img.src = 'gatlingKitteh.jpg';
-});
+var blackKitteh = new Player('Black Kitteh');
+blackKitteh.img = new Image('images/blackKitteh.png');
+blackKitteh.img.src = 'images/blackKitteh.png';
 
-var blob = new Monster({
-	name: 'The Blob';
-	var img = new Image(); 
-	img.src = 'blob.png';
 
-});
+var gatlingKitteh = new Player('Gatling Kitteh');
+gatlingKitteh.img = new Image('images/gatlingKitteh.jpg');
+gatlingKitteh.img.src = 'images/gatlingKitteh.jpg';
+$(gatlingKitteh).click(function (){
+	$("#fight").show();
+})	
 
-var Alien = new Monster({
-	name: 'The Alien';
-	var img = new Image(); 
-	img.src = 'alien.png';
-});
+var blob = new Monster(25,'The Blob');
+blob.img = new Image('images/blob.png');
+blob.img.src = 'images/blob.png';
 
-// e.g. var purplePeopleEater = new Monster();
-// e.g. var tinaFey = new Player();
-// e.g. var attackMode = function(target){ Some code that produces an attack - pew, pew! };
-
+var alien = new Monster(50, 'The Alien');
+alien.img = new Image('images/alien.png');
+alien.img.src = 'images/alien.png';
 
 
 // ACTIONS
 // This is where jQuery will come into play and where you'll set a lot of your
 // interactions for players and monsters. (e.g. player selection, attack interactions)
 // e.g. $('.purple-people-eater').click(function () { Some code that attacks the monster! };
-
-
  
-
 
 //compile handlebars template
 /*var source   = $("#select").html();
